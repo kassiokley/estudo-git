249dd217e27f404811f99ab99778fad2a2185e35
kassiokley <seloseishiro@gmail.com>
2020-05-15 02:44:38 -0300

    "Começo da API"

---
+++ api-express/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  usarname: 'postgres',
+  password: 'bcc123',
+  database: 'sqlnode',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
--- api-express/src/server.js
+++ api-express/src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/teste', (req, res) => {
-  return res.json({hello: 'world 2'})
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
